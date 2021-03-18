bb8c271fcc9819f266907266c251d73154bd1727
Veronika Dodda <v.shengeliya@gmail.com>
2021-03-18 12:49:45 -0400

    rendered burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import Aux from '../../hoc/Aux'
+import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends React.Component {
 
     render(){
         return(
             <Aux>
-                <div>Burger</div>
+                <Burger/>
                 <div>Build Control</div>
             </Aux>
         );
