c370102ee8db53281978f3c68714a0c81f1d69cb
Veronika Dodda <v.shengeliya@gmail.com>
2021-03-18 12:20:14 -0400

    burger ingredient css and js added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
-import Layout from './components/Layout/Layout';
+import React from 'react';
 
-class App extends Component {
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+
+class App extends React.Component {
   render() {
     return (
       <div>
         <Layout>
-          <p>Test</p>
+          <BurgerBuilder/>
           </Layout>
       </div>
     );
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Aux from '../../hoc/Aux'
+
+class BurgerBuilder extends React.Component {
+
+    render(){
+        return(
+            <Aux>
+                <div>Burger</div>
+                <div>Build Control</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
