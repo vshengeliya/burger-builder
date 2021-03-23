5e0d5e178c554ebb79f10120bf2c9c21e7df0481
Veronika Dodda <v.shengeliya@gmail.com>
2021-03-23 11:33:33 -0400

    add ingedient function added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,21 +4,51 @@ import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon:0.7
+}
+
+
+
 class BurgerBuilder extends React.Component {
     state ={
         ingredients: {
-            salad: 1,
+            salad: 0,
             bacon:0,
             cheese: 0,
             meat: 0
-        }
+        },
+
+        totalPrive: 4
     }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updtedIngredients = {
+            ...this.state.ingredients
+        };
+
+        updtedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+
+        this.setState({totalPrice: newPrice, ingredients: updtedIngredients})
+        
+    }
+
+    removeIngredientHandler
 
     render(){
         return(
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <BuildControls/>
+                <BuildControls 
+                ingredientAdded = {this.addIngredientHandler}/>
             </Aux>
         );
     }
