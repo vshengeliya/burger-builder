81aa8db356012681357af9e9d2b9630dea8351a7
Veronika Dodda <v.shengeliya@gmail.com>
2021-03-25 16:44:02 -0400

    toolbar added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -103,7 +103,8 @@ class BurgerBuilder extends React.Component {
                     <OrderSummary 
                     ingredients={this.state.ingredients}
                     purchaseCancelled={this.purchaseCancelHandler}
-                    purchaseContinued={this.purchaseContinueHandler}/>
+                    purchaseContinued={this.purchaseContinueHandler}
+                    price={this.state.totalPrice}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
