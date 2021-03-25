0139b4d9924fdcb2bf5bfa13b6cac0ce223d6088
Veronika Dodda <v.shengeliya@gmail.com>
2021-03-25 16:25:34 -0400

    button added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -78,8 +78,12 @@ class BurgerBuilder extends React.Component {
         this.setState({purchasing:true})
     }
 
-    purchaseCancelHandler = ()=>{
+    purchaseCancelHandler = () =>{
         this.setState({purchasing:false})
+    }
+
+    purchaseContinueHandler = () =>{
+        alert('You continue!');
     }
 
     render(){
@@ -96,7 +100,10 @@ class BurgerBuilder extends React.Component {
                 <Modal 
                 show={this.state.purchasing}
                 modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients}/>
+                    <OrderSummary 
+                    ingredients={this.state.ingredients}
+                    purchaseCancelled={this.purchaseCancelHandler}
+                    purchaseContinued={this.purchaseContinueHandler}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
