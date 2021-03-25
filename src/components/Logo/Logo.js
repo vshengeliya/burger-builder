71644b3fd59a851fa688414aaa3d160a352e83bc
Veronika Dodda <v.shengeliya@gmail.com>
2021-03-25 17:07:25 -0400

    logo in progress

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import burgerLogo from '../../assets/images/burger-logo.png'
+
+const logo = (props) => (
+    <div>
+        <img  src={burgerLogo}>
+    </div>
+);
+
+export default logo;
